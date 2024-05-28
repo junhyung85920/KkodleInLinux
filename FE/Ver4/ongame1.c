9f985d1a5daec2f332260afba998f1f25a145b81
With_jo <researcherdragon@gmail.com>
2024-05-28 11:25:42 +0900

    test용 ongame

---
+++ FE/Ver4/ongame1.c
@@ -0,0 +1,215 @@
+
+
+#include "drawlib.h"
+
+
+int printMenu(int row, int col);
+
+
+// Socket communication functions
+int connectToServer(const char *server_ip, int server_port);
+int sendAnswer(int sockfd, Word *answer);
+int receiveAnswer(int sockfd, Word *answer);
+
+
+void onGame(char *path)
+{
+
+    int menu = 0, opponent_result=3, count = 0, round = 0, result = -1;
+    char c;
+    Word *answer, *answer2;
+    Word opponent_answer[6] = {Q,H,W,K,R,L};
+    Word opponent_answer2[6] = {W,J,D,E,K,Q};
+
+    int sockfd = -1;
+
+    printMenu(16, 14);
+
+    sockfd = connectToServer(path, 8888); // Replace with actual server IP and port
+    if (sockfd < 0)
+    {
+        printError("Failed to connect to the server.");
+        return;
+    }
+
+    sendAnswer(sockfd, opponent_answer);
+    receiveAnswer(sockfd, answer);
+
+    clear();
+    for (int i = 0; i < 6; i++)
+    {
+        printWord(answer[i], 2 + 0 * 7, 6 + 13 * i);
+    }
+
+    sendAnswer(sockfd, opponent_answer2);
+    receiveAnswer(sockfd, answer2);
+
+    for (int i = 0; i < 6; i++)
+    {
+        printWord(answer2[i], 2 + 1 * 7, 6 + 13 * i);
+    }
+    getch();
+    // close socket
+    if (sockfd >= 0)
+    {
+        close(sockfd);
+    }
+
+    return;
+}
+int connectToServer(const char *server_ip, int server_port)
+{
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("socket");
+        return -1;
+    }
+    struct hostent *host = gethostbyname(server_ip);
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(server_port);
+    bcopy((void *)host->h_addr, (void *)&serv_addr.sin_addr, host->h_length);
+
+    /*
+    if (inet_pton(AF_INET, server_ip, &serv_addr.sin_addr) <= 0)
+    {
+        perror("inet_pton");
+        close(sockfd);
+        return -1;
+    }
+    */
+
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        perror("connect");
+        close(sockfd);
+        return -1;
+    }
+
+    return sockfd;
+}
+
+int sendAnswer(int sockfd, Word *answer)
+{
+    if (send(sockfd, answer, sizeof(Word) * 6, 0) < 0)
+    {
+        perror("send");
+        return -1;
+    }
+    return 0;
+}
+
+int receiveAnswer(int sockfd, Word *answer)
+{
+    if (recv(sockfd, answer, sizeof(Word) * 6, 0) < 0)
+    {
+        printf("recv failed\n");
+        perror("recv");
+        return -1;
+    }
+    return 0;
+}
+
+/////////
+
+int printMenu(int row, int col)
+{
+    clear();
+
+    col += 7;
+
+    move(row, col);
+    printw("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s",
+           u8"\u250F", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2513");
+    move(row + 1, col);
+    printw("%s", u8"\u2503");
+    move(row + 1, col + 47);
+    printw("%s", u8"\u2503");
+    move(row + 2, col);
+    printw("%s", u8"\u2503");
+    move(row + 2, col + 47);
+    printw("%s", u8"\u2503");
+    move(row + 3, col);
+    printw("%s", u8"\u2503");
+    move(row + 3, col + 47);
+    printw("%s", u8"\u2503");
+    move(row + 4, col);
+    printw("%s", u8"\u2503");
+    move(row + 4, col + 47);
+    printw("%s", u8"\u2503");
+    move(row + 5, col);
+    printw("%s", u8"\u2503");
+    move(row + 5, col + 47);
+    printw("%s", u8"\u2503");
+
+    printWord(A, row, col);
+    printWord(J, row, col + 9);
+    printWord(F, row, col + 20);
+    printWord(X, row, col + 31);
+    printWord(L, row, col + 39);
+
+    move(row + 6, col);
+    printw("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s",
+           u8"\u2517", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u251B");
+
+    row += 16;
+    col -= 7;
+
+    move(row, col);
+    printw("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s",
+           u8"\u250F", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2513");
+
+    move(row + 1, col);
+    printw("%s", u8"\u2503");
+    move(row + 1, col + 62);
+    printw("%s", u8"\u2503");
+    move(row + 2, col);
+    printw("%s", u8"\u2503");
+    move(row + 2, col + 62);
+    printw("%s", u8"\u2503");
+    move(row + 3, col);
+    printw("%s", u8"\u2503");
+    move(row + 3, col + 62);
+    printw("%s", u8"\u2503");
+    move(row + 4, col);
+    printw("%s", u8"\u2503");
+    move(row + 4, col + 62);
+    printw("%s", u8"\u2503");
+    move(row + 5, col);
+    printw("%s", u8"\u2503");
+    move(row + 5, col + 62);
+    printw("%s", u8"\u2503");
+
+    printWord(T, row, col);
+    printWord(L, row, col + 8);
+    printWord(D, row, col + 16);
+    printWord(R, row, col + 28);
+    printWord(M, row, col + 39);
+    printWord(F, row, col + 50);
+
+    move(row + 6, col);
+    printw("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s",
+           u8"\u2517", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u2501", u8"\u251B");
+    refresh();
+
+    char m;
+
+    while (1)
+    {
+        m = getch();
+        if (m == 'M' || m == 'm')
+        {
+            return 1;
+        }
+        else if (m == 'S' || m == 's')
+        {
+            return 0;
+        }
+        else
+        {
+            printError("Usage : multi is 'M', single is 'S'");
+        }
+    }
+}
