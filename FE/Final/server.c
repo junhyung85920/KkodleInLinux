9f2eb425fe81c59088635709beb6a044373baab1
junhyung85920 <junhyung85920@gmail.com>
2024-05-29 13:50:24 +0900

    trim server.c

--- FE/Final/server.c
+++ FE/Final/server.c
@@ -25,6 +25,8 @@ int messages_received = 0;
 Word word_arrays[MAX_CLIENTS][ARRAY_SIZE];
 int int_values[MAX_CLIENTS];
 
+
+// thread 실행 함수
 void *handle_client(void *arg) {
     ClientData *client_data = (ClientData *)arg;
     int client_socket = client_data->socket;
@@ -56,16 +58,16 @@ void *handle_client(void *arg) {
             printf("\n");
 
             if (messages_received == MAX_CLIENTS) {
-                // 모든 클라이언트로부터 배열을 받았을 때
+                // when two clients send word arrays
                 for (int i = 0; i < MAX_CLIENTS; ++i) {
                     int other_index = (i + 1) % MAX_CLIENTS;
                     send(client_sockets[i], word_arrays[other_index], sizeof(word_arrays[other_index]), 0);
                     printf("Sent word array from Client %d to Client %d\n", other_index + 1, i + 1);
                 }
-                messages_received = 0; // 메시지 수신 카운트 초기화
-                memset(word_arrays, 0, sizeof(word_arrays)); // 배열 초기화
+                messages_received = 0; // init message count
+                memset(word_arrays, 0, sizeof(word_arrays)); // init buffer(word)
 
-                // 모든 클라이언트의 첫 번째 메시지 상태를 false로 설정
+                // is_first_message -> false로 설정 (다음 메시지부터는 int value를 받음)
                 for (int i = 0; i < MAX_CLIENTS; ++i) {
                     client_data->is_first_message = 0;
                 }
@@ -88,15 +90,15 @@ void *handle_client(void *arg) {
             messages_received++;
 
             if (messages_received == MAX_CLIENTS) {
-                // 모든 클라이언트로부터 값을 받았을 때
+                // when two clients send int values
                 for (int i = 0; i < MAX_CLIENTS; ++i) {
                     int other_index = (i + 1) % MAX_CLIENTS;
                     int send_value = htonl(int_values[other_index]);
                     send(client_sockets[i], &send_value, sizeof(send_value), 0);
                     printf("Sent int value from Client %d to Client %d: %d\n", other_index + 1, i + 1, int_values[other_index]);
                 }
-                messages_received = 0; // 메시지 수신 카운트 초기화
-                memset(int_values, 0, sizeof(int_values)); // 값 초기화
+                messages_received = 0; // init message count 
+                memset(int_values, 0, sizeof(int_values)); // init buffer(int)
             }
             pthread_mutex_unlock(&lock);
         }
@@ -108,36 +110,36 @@ int main() {
     struct sockaddr_in address;
     int addrlen = sizeof(address);
 
-    // 클라이언트 소켓 배열 초기화
+    // init array of client sockets
     for (int i = 0; i < MAX_CLIENTS; ++i) {
         client_sockets[i] = 0;
     }
 
-    // 뮤텍스 초기화
+    // init mutex
     if (pthread_mutex_init(&lock, NULL) != 0) {
         perror("pthread_mutex_init");
         exit(EXIT_FAILURE);
     }
 
-    // 소켓 파일 디스크립터 생성
+    // socket fd 생성
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // 주소 구조체 설정
+    // address struct 설정
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // 소켓에 주소 바인딩
+    // bind
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("bind failed");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
 
-    // 수신 대기 모드 설정
+    // listen
     if (listen(server_fd, 3) < 0) {
         perror("listen");
         close(server_fd);
@@ -147,23 +149,22 @@ int main() {
     printf("Waiting for clients...\n");
 
     while (1) {
-        // 새 클라이언트 연결 수락
+        // accept clients
         if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
             perror("accept");
             exit(EXIT_FAILURE);
         }
 
-        // 뮤텍스 잠금
         pthread_mutex_lock(&lock);
 
-        // 빈 슬롯 찾기
+        // add new socket to array of client sockets if not full
         int i;
         for (i = 0; i < MAX_CLIENTS; ++i) {
             if (client_sockets[i] == 0) {
                 client_sockets[i] = new_socket;
                 printf("Client %d connected\n", i + 1);
 
-                // 클라이언트 처리 스레드 생성
+                // client thread 생성
                 pthread_t thread_id;
                 ClientData *client_data = (ClientData *)malloc(sizeof(ClientData));
                 client_data->socket = new_socket;
@@ -176,16 +177,16 @@ int main() {
                     client_sockets[i] = 0;
                     free(client_data);
                 } else {
-                    pthread_detach(thread_id); // 스레드가 종료되면 리소스 자동 회수
+                    pthread_detach(thread_id); // 스레드가 종료 시 리소스 thread 해제
                 }
-
                 break;
             }
         }
-
-        // 뮤텍스 잠금 해제
         pthread_mutex_unlock(&lock);
     }
+
+
+
 
     close(server_fd);
     pthread_mutex_destroy(&lock);
